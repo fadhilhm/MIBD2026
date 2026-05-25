2fc749ac0dad2ffec95289c7a5ab255b21cdeb00
fadhilhm <fadhillah.ilham71@gmail.com>
2026-05-25 18:03:21 +0700

    feat(db): connect to MS SQL

--- index.js
+++ index.js
@@ -1,12 +1,45 @@
+const sql = require('mssql');
 const express = require('express');
 const path = require('path');
-const app = express();
 const port = 3000;
+
 const PATHS = {
     html: path.join(__dirname, 'HTML'),
     css: path.join(__dirname, 'CSS'),
     scripts: path.join(__dirname, 'Scripts')
 };
+
+const sqlConfig = {
+    user: 'adminTester',
+    password: 'admin1234',
+    server: 'localhost',
+    database: 'CarRentalDB',
+    port: 1433,
+    options: {
+        encrypt: false,
+        trustServerCertificate: true
+    }
+}
+
+const app = express();
+
+// connect ms sql
+async function connectMS_SQL() {
+    try {
+        console.log("Connecting to SQL Server...");
+        let pool = await sql.connect(sqlConfig);
+        console.log("Connected to SQL Server!");
+
+        let result = await pool.request().query("SELECT @@VERSION as SQL_Version");
+        console.log("\nQuery Test Result:");
+        console.table(result.recordset);
+
+        await sql.close();
+    } catch (error) {
+        console.error("Database connection failed :(\n", error);
+    }
+}
+connectMS_SQL();
 
 // serve static folder
 app.use(express.static(PATHS.html, { extensions: ['html'] }));
