752d72638cda90e253a7919306133fffceebbabd
fadhilhm <fadhillah.ilham71@gmail.com>
2026-06-06 14:14:54 +0700

    katalog mobil data cabangnya udah bener

--- Scripts/katalogMobil.js
+++ Scripts/katalogMobil.js
@@ -130,8 +130,6 @@ productContainer.addEventListener('click', (e) => {
 
         console.log(mobilTerpilih);
 
-        document.getElementById('popup-img').src = `/image/${mobilTerpilih.NamaMerek.toLowerCase()}_${mobilTerpilih.NamaTipe.toLowerCase()}_${mobilTerpilih.Nopol.trim()}.png`;
-        document.getElementById('popup-img').alt = `${mobilTerpilih.NamaMerek} ${mobilTerpilih.NamaTipe}`;
         document.getElementById('popup-title').innerText = `${mobilTerpilih.NamaMerek}`;
         document.getElementById('popup-cabang').innerText = `${mobilTerpilih.NamaCabang}, ${mobilTerpilih.NamaJalan}`;
         document.getElementById('popup-nopol').innerText = `${mobilTerpilih.Nopol}`;
@@ -139,6 +137,8 @@ productContainer.addEventListener('click', (e) => {
         document.getElementById('popup-kapasitas').innerText = `${mobilTerpilih.Kapasitas} Kursi`;
         document.getElementById('popup-tahun-keluaran').innerText = mobilTerpilih.TahunPembuatan;
         document.getElementById('popup-tipe').innerText = `${mobilTerpilih.NamaTipe}`;
+        document.getElementById('popup-email-cabang').innerText = mobilTerpilih.AlamatEmail;
+        document.getElementById('popup-no-telp-cabang').innerText = `${mobilTerpilih.NoTelp}`;
 
         popupOverlay.classList.add("active");
     }
