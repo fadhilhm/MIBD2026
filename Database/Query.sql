f3440c51649e412469aa71e52c4bea97d249694a
Eclipse73 <pearcenathaniel73@gmail.com>
2026-06-06 14:59:48 +0700

    API Peminjaman

--- Database/Query.sql
+++ Database/Query.sql
@@ -1,35 +1,7 @@
-SELECT *FROM [USER]
-SELECT *FROM PEGAWAI
-SELECT *FROM [MEMBER]
-SELECT *FROM MOBIL
-SELECT * FROM CABANG
-SELECT * FROM Merek_mobil
-SELECT * FROM Foto
-SELECT * FROM Peminjaman
-SELECT * FROM Tipe_Mobil
 
--- Delete rows from table 'TableName'
-DELETE FROM [USER]
-GO
 
--- Log In
-SELECT EMAIL_USER.AlamatEmail, [USER].UserPassword
-FROM [USER]
-    INNER JOIN EMAIL_USER
-    ON [USER].IDUser = EMAIL_USER.IDUser
 
--- Dummy Data
--- DROP DB dulu
-USE master;
-GO
-
-ALTER DATABASE CarRentalDB 
-SET SINGLE_USER 
-WITH ROLLBACK IMMEDIATE;
-GO
-
-DROP DATABASE CarRentalDB;
-GO
+-- =======
 
 -- =======================================
 -- Query show all Peminjaman di Dashboard-Pegawai pada Cabangnya
@@ -43,15 +15,17 @@ SELECT [USER].Nama,
     PEMINJAMAN.Nopol,
     PEMINJAMAN.TanggalPeminjaman,
     PEMINJAMAN.TanggalBatasPengembalian,
+    PEMINJAMAN.TanggalKembali
+-- PEMINJAMAN.TotalBiaya + (PEMINJAMAN.TotalBiaya * PEMINJAMAN.PersentaseDenda /100.0) AS TotalBiayaPlusDenda
     PEMINJAMAN.TanggalKembali,
     PEMINJAMAN.TotalBiaya + (PEMINJAMAN.TotalBiaya * PEMINJAMAN.PersentaseDenda /100.0) AS TotalBiayaPlusDenda
 FROM PEMINJAMAN
-    JOIN MEMBER ON PEMINJAMAN.IDMember = MEMBER.IDUser
-    JOIN MOBIL ON PEMINJAMAN.Nopol = MOBIL.NOPOL
-    JOIN MEREK_MOBIL ON MOBIL.IDMerek = MEREK_MOBIL.IDMerek
-    JOIN [USER] ON [USER].IDUser = MEMBER.IDUser
-WHERE PEMINJAMAN.IDPegawai IN (
-    SELECT PEGAWAI.IDUser
-FROM PEGAWAI JOIN CABANG ON PEGAWAI.IDCabang = CABANG.IDCabang
-WHERE CABANG.NamaCabang = 'Cabang Citarum');
+    JOIN MEMBER ON PEMINJAMAN.IDMember = MEMBER.IDUser -- Cari idMember utk Nama
+    JOIN [USER] ON [USER].IDUser = MEMBER.IDUser -- Cari Nama User
+    JOIN MOBIL ON PEMINJAMAN.Nopol = MOBIL.NOPOL -- Cari idMobiil utk Nama Merek
+    JOIN MEREK_MOBIL ON MOBIL.IDMerek = MEREK_MOBIL.IDMerek -- Cari Nama Merek
+    JOIN PEGAWAI ON PEGAWAI.IDCabang = MOBIL.IDCabang
+WHERE PEGAWAI.IDCabang = MOBIL.IDCabang
 GO
+
+
--- Scripts/sign-up.js
+++ Scripts/sign-up.js
@@ -8,12 +8,12 @@ document.querySelector('.form-field').addEventListener('submit', async (e) => {
     const phone = document.getElementById('phone').value;
     const noSIM = document.getElementById('no-sim').value;
     const password = document.getElementById('password').value;
-    const form = { nama, jenisKelamin, tanggalLahir, email, phone, noSIM, password};
+    const form = { nama, jenisKelamin, tanggalLahir, email, phone, noSIM, password };
 
     try {
         const response = await fetch('api/register', {
             method: 'POST',
-            headers: { 
+            headers: {
                 'Content-Type': 'application/json'
             },
             body: JSON.stringify(form)
