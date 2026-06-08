fa69415d6a1fbb847f7c33a47864a80714b9511c
esevel18 <eugeniusstevenleif18@gmail.com>
2026-06-08 20:22:00 +0700

    revert

--- server-routes/peminjaman.js
+++ server-routes/peminjaman.js
@@ -152,8 +152,6 @@ router.get('/peminjaman', async (req, res) => {
     }
 });
 
-module.exports = router;
-
 
 router.get('/get-riwayat-rental', async (req, res) => {
     try {
@@ -228,4 +226,225 @@ router.post('/booking', async (req, res) => {
     }
 });
 
+/**
+ * Query Pop Up Pegawai (Konfirmasi, Tindakan)
+ * Author: Pearce Nathaniel N.
+ */
+async function querySubmitKonfirmasi(idPegawaiAktif, nopol, idMember, tglPeminjaman, fotoDepan, fotoBelakang, fotoKanan, fotoKiri) {
+    const pool = await getPool();
+    const transaction = new sql.Transaction(pool);
+
+    try {
+        await transaction.begin();
+        const request = new sql.Request(transaction);
+
+        const cleanDate = new Date(tglPeminjaman).toISOString().split('T')[0];
+
+        request.input('idPegawai', sql.Int, idPegawaiAktif);
+        request.input('nopol', sql.VarChar, nopol)
+        request.input('idMember', sql.Int, idMember)
+        request.input('tglPeminjaman', sql.Date, cleanDate);
+
+        const queryUpdate = `
+            UPDATE PEMINJAMAN
+            SET IDPegawai = @idPegawai
+            WHERE Nopol = @nopol
+            AND IDMember = @idMember
+            AND TanggalPeminjaman = @tglPeminjaman;
+            `;
+        await request.query(queryUpdate);
+        // Input Link Foto dari Param
+        const fotoArray = [
+            { url: fotoDepan, desc: 'Foto Depan Sebelum Sewa' },
+            { url: fotoBelakang, desc: 'Foto Belakang Sebelum Sewa' },
+            { url: fotoKanan, desc: 'Foto Kanan Sebelum Sewa' },
+            { url: fotoKiri, desc: 'Foto Kiri Sebelum Sewa' }
+        ];
+
+
+        for (let i = 0; i < fotoArray.length; i++) {
+            if (fotoArray[i].url) {
+                // console.log(`-> Mengeksekusi INSERT FOTO ke-${i}: ${fotoArray[i].desc}`);
+                const imgRequest = new sql.Request(transaction);
+                imgRequest.input('idMember', sql.Int, idMember);
+                imgRequest.input('idPegawai', sql.Int, idPegawaiAktif);
+                imgRequest.input('nopol', sql.VarChar, nopol);
+                imgRequest.input('gambar', sql.VarChar(2048), fotoArray[i].url);
+                imgRequest.input('kondisi', sql.Bit, 0); // 0 = Sebelum
+                imgRequest.input('deskripsi', sql.Text, fotoArray[i].desc);
+
+                const queryInsertFoto = `
+                    INSERT INTO FOTO (IDMember, IDPegawai, Nopol, Gambar, Kondisi, Deskripsi)
+                    VALUES (@idMember, @idPegawai, @nopol, @gambar, @kondisi, @deskripsi);
+                    `;
+                await imgRequest.query(queryInsertFoto);
+            }
+            // } else {
+                // console.log(`-> Foto indeks ke-${i} dilewati karena kosong/undefined.`);
+        }
+
+        await transaction.commit();
+        return { success: true };
+    } catch (error) {
+        await transaction.rollback();
+        throw error;
+    }
+}
+
+router.post('/konfirmasi', async (req, res) => {
+    const {
+        nopol,
+        idMember,
+        tglPeminjaman,
+        fotoDepan,
+        fotoBelakang,
+        fotoKanan,
+        fotoKiri
+    } = req.body;
+
+    const idPegawaiAktif = req.session.idUser;
+    // Validasi session proteksi
+    if (!idPegawaiAktif) {
+        return res.status(401).json({ success: false, message: "Sesi habis, silahkan login kembali." });
+    }
+
+    // Validasi data body
+    if (!nopol || !idMember || !tglPeminjaman) {
+        return res.status(400).json({ success: false, message: "Parameter transaksi tidak lengkap." });
+    }
+
+    try {
+        const records = await querySubmitKonfirmasi(
+            idPegawaiAktif,
+            nopol,
+            parseInt(idMember),
+            tglPeminjaman,
+            fotoDepan,
+            fotoBelakang,
+            fotoKanan,
+            fotoKiri
+        );
+        return res.status(200).json({
+            success: true,
+            message: "Transaksi peminjaman berhasil dikonfirmasi ke status Ongoing."
+        });
+    } catch (error) {
+        console.error("SQL Server Error [Konfirmasi]:", error);
+        return res.status(500).json({
+            success: false,
+            message: "Gagal memperbarui database status peminjaman."
+        });
+    }
+});
+
+
+// Tindakan Pengembalian
+async function querySubmitTindakan(nopol, idMember, tglPeminjaman, totalDenda, idPegawaiAktif, fotoDepan, fotoBelakang, fotoKanan, fotoKiri) {
+    const pool = await getPool();
+    const transaction = new sql.Transaction(pool);
+
+    try {
+        await transaction.begin();
+        const request = new sql.Request(transaction);
+
+
+        const cleanDatePeminjaman = new Date(tglPeminjaman).toISOString().split('T')[0];
+        const cleanDateKembali = new Date().toISOString().split('T')[0];
+
+        request.input('nopol', sql.VarChar, nopol);
+        request.input('idMember', sql.Int, idMember);
+        request.input('tglPeminjaman', sql.Date, cleanDatePeminjaman);
+        request.input('tanggalKembali', sql.Date, cleanDateKembali);
+        request.input('totalDenda', sql.Decimal(12, 2), totalDenda);
+
+        const queryUpdatePeminjaman = `
+                UPDATE PEMINJAMAN
+                SET 
+                    TanggalKembali = @tanggalKembali,
+                    TotalDenda = @totalDenda
+                WHERE Nopol = @nopol
+                AND IDMember = @idMember
+                AND TanggalPeminjaman = @tglPeminjaman;
+                `;
+        const updateResult = await request.query(queryUpdatePeminjaman);
+        // console.log(`=== [DEBUG SQL TINDAKAN] Rows Affected: ${updateResult.rowsAffected[0]} ===`);
+
+        const fotoArray = [
+            { url: fotoDepan, desc: 'Foto Depan Sesudah Sewa' },
+            { url: fotoBelakang, desc: 'Foto Belakang Sesudah Sewa' },
+            { url: fotoKanan, desc: 'Foto Kanan Sesudah Sewa' },
+            { url: fotoKiri, desc: 'Foto Kiri Sesudah Sewa' }
+        ];
+
+        for (let i = 0; i < fotoArray.length; i++) {
+            if (fotoArray[i].url) {
+                // console.log(`-> Mengeksekusi INSERT FOTO ke-${i}: ${fotoArray[i].desc}`);
+                const imgRequest = new sql.Request(transaction);
+                imgRequest.input('idMember', sql.Int, idMember);
+                imgRequest.input('idPegawai', sql.Int, idPegawaiAktif);
+                imgRequest.input('nopol', sql.VarChar, nopol);
+                imgRequest.input('gambar', sql.VarChar(2048), fotoArray[i].url);
+                imgRequest.input('kondisi', sql.Bit, 1); // 1 = Sesudah
+                imgRequest.input('deskripsi', sql.Text, fotoArray[i].desc);
+
+                const queryInsertFoto = `
+                        INSERT INTO FOTO (IDMember, IDPegawai, Nopol, Gambar, Kondisi, Deskripsi)
+                        VALUES (@idMember, @idPegawai, @nopol, @gambar, @kondisi, @deskripsi);
+                        `;
+                await imgRequest.query(queryInsertFoto);
+            }
+            // } else {
+            //     console.log(`-> Foto indeks ke-${i} dilewati karena kosong/undefined.`);
+            // }
+        }
+
+        await transaction.commit();
+        return updateResult;
+
+    } catch (error) {
+        await transaction.rollback();
+        throw error;
+    }
+}
+
+router.post('/tindakan', async (req, res) => {
+    const {
+        nopol,
+        idMember,
+        tglPeminjaman,
+        totalDenda,
+        fotoDepan,
+        fotoBelakang,
+        fotoKanan,
+        fotoKiri
+    } = req.body;
+
+    const idPegawaiAktif = req.session.idUser;
+
+    if (!idPegawaiAktif) {
+        return res.status(401).json({ success: false, message: "Sesi habis, silakan login kembali." });
+    }
+    if (!nopol || !idMember || !tglPeminjaman) {
+        return res.status(400).json({ success: false, message: "Parameter tidak lengkap." });
+    }
+
+    try {
+
+        // console.log("=== [DEBUG BACKEND RECEIVED] DATA MASUK ===");
+        // console.log(req.body);
+
+        const result = await querySubmitTindakan(nopol, parseInt(idMember), tglPeminjaman, parseFloat(totalDenda) || 0, idPegawaiAktif, fotoDepan, fotoBelakang, fotoKanan, fotoKiri);
+
+        // console.log("=== [DEBUG SQL RESULT] ROWS AFFECTED ===");
+        // console.log(result.rowsAffected);
+        
+        return res.status(200).json({
+            success: true,
+            message: "Mobil berhasil dikembalikan dan denda dicatat!"
+        });
+    } catch (error) {
+        console.error("SQL Server Error [Konfirmasi]:", error);
+        return res.status(500).json({ success: false, message: "Gagal menyimpan data pengembalian ke SQL Server." });
+    }
+});
 module.exports = router;
